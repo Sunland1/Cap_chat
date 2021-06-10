ac99e339966469846d73886585587f564883692e
Sunland <alexandre.horville@outlook.fr>
2021-06-10 12:57:15 +0200

    LAST RUN

--- Back/model/Model.js
+++ Back/model/Model.js
@@ -310,6 +310,7 @@ class Model {
     }
 
 
+
 }
 
 
--- CapChatFront/index.js
+++ CapChatFront/index.js
@@ -1,6 +1,7 @@
 let isVisible = false;
 let timer = 30;
 let imageSelectValue
+let previousScale
 
 
 $(document).ready(function (){
@@ -8,6 +9,20 @@ $(document).ready(function (){
     //gestion des evènement
     $('img').click(function(){
         imageSelectValue = $(this).data('id')
+        if(previousScale !== undefined){
+            previousScale.css({
+                "transition-duration": "500ms",
+                "transition-property": "transform",
+                "transform": "scale(1,1)"
+            })
+        }
+        $(this).css({
+            "transition-duration": "500ms",
+            "transition-property": "transform",
+            "transform": "scale(0.8,0.8)"
+        })
+        previousScale = $(this)
+
     })
 
 })
